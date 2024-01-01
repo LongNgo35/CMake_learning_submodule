22ff8a8a01928b32aca9a4cffaa0d8d1ba1ad0f9
LongNgo35 <118904539+LongNgo35@users.noreply.github.com>
2024-01-01 10:22:43 +0700

    upload hello world library for submodule

---
+++ lib_hello_world_git_repos_submodule.c
@@ -0,0 +1,6 @@
+#include "lib_hello_world_git_repos.h"
+
+void hello_world_printing_git_repos_submodule(void)
+{
+	printf("hello world - called by lib function - cloned form git repository with submodule \n");
+}
---
+++ lib_hello_world_git_repos_submodule.h
@@ -0,0 +1,11 @@
+#ifndef __LIB_HELLO_WORLD_GIT_REPOS_SUB_MODULE__
+#define __LIB_HELLO_WORLD_GIT_REPOS_SUB_MODULE__
+
+#include <stdio.h>
+
+/* prototype */
+void hello_world_printing_git_repos_submodule(void);
+
+#endif
+
+
